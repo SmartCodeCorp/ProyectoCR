ece598aac36ad00cc1a4928ca72db166cca82bbc
MayraniRG03 <Mayrani.rudegirl1997@gmail.com>
2018-07-20 21:18:55 -0500

    nuevas actualizaciones

--- application/views/FrontEnd/vw_login.php
+++ application/views/FrontEnd/vw_login.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="<?=base_url().'FrontEnd/Template/img/icons/logoR.ico';?>">
 	<link rel="icon" type="image/png" href="<?=base_url();?>/FrontEnd/Login/images/profile.png"/>
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?=base_url();?>FrontEnd/Login/vendor/bootstrap/css/bootstrap.min.css">
